639fcdc5ded946c51116b739196c330992c089d4
peterdm <peterdixonmoses@gmail.com>
2012-01-15 16:41:02 -0500

    Recursive

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 function snip(data) {
 	data = data.replace(/<!-- snip:[/]?([^> ]+)[^>]* -->/g, function(match, submatch){
 		console.log("File to snip out:"+submatch);
-		return fs.readFileSync('snips/'+submatch, 'utf8');
+		return snip(fs.readFileSync('snips/'+submatch, 'utf8'));
 		});
 	
 //	console.log(data);
